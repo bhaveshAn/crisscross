c32ea27c0898bf0afd3c077e919fe69ea95ca35d
bhaveshAn <bhaveshanand7@gmail.com>
2017-05-23 22:32:45 +0530

    Added compat.py

---
+++ crisscross/__init__.py
@@ -0,0 +1,89 @@
+'''
+Crisscross
+==========
+
+'''
+
+__all__ = ('accelerometer', 'audio', 'barometer', 'battery', 'call', 'camera',
+           'compass', 'email', 'filechooser', 'flash', 'gps', 'gravity',
+           'gyroscope', 'irblaster', 'light', 'orientation', 'notification',
+           'proximity', 'sms', 'tts', 'uniqueid', 'vibrator', 'wifi',
+           'temperature')
+
+__version__ = '1.0.0dev'
+
+
+from crisscross import facades
+from crisscross.utils import Proxy
+
+#: Accelerometer proxy to :class:`plyer.facades.Accelerometer`
+accelerometer = Proxy('accelerometer', facades.Accelerometer)
+
+#: Audio proxy to :class:`plyer.facades.Audio`
+audio = Proxy('audio', facades.Audio)
+
+#: Barometer proxy to :class:`plyer.facades.Barometer`
+barometer = Proxy('barometer', facades.Barometer)
+
+#: Battery proxy to :class:`plyer.facades.Battery`
+battery = Proxy('battery', facades.Battery)
+
+#: Call proxy to  :class `plyer.facades.Call`
+call = Proxy('call', facades.Call)
+
+#: Compass proxy to :class:`plyer.facades.Compass`
+compass = Proxy('compass', facades.Compass)
+
+#: Camera proxy to :class:`plyer.facades.Camera`
+camera = Proxy('camera', facades.Camera)
+
+#: Email proxy to :class:`plyer.facades.Email`
+email = Proxy('email', facades.Email)
+
+#: FileChooser proxy to :class:`plyer.facades.FileChooser`
+filechooser = Proxy('filechooser', facades.FileChooser)
+
+#: GPS proxy to :class:`plyer.facades.GPS`
+gps = Proxy('gps', facades.GPS)
+
+#: Gravity proxy to :class:`plyer.facades.Gravity`
+gravity = Proxy('gravity', facades.Gravity)
+
+#: Gyroscope proxy to :class:`plyer.facades.Gyroscope`
+gyroscope = Proxy('gyroscope', facades.Gyroscope)
+
+#: IrBlaster proxy to :class:`plyer.facades.IrBlaster`
+irblaster = Proxy('irblaster', facades.IrBlaster)
+
+#: Light proxy to :class:`plyer.facades.Light`
+light = Proxy('light', facades.Light)
+
+#: Orientation proxy to :class:`plyer.facades.Orientation`
+orientation = Proxy('orientation', facades.Orientation)
+
+#: Notification proxy to :class:`plyer.facades.Notification`
+notification = Proxy('notification', facades.Notification)
+
+#: Proximity proxy to :class:`plyer.facades.Proximity`
+proximity = Proxy('proximity', facades.Proximity)
+
+#: Sms proxy to :class:`plyer.facades.Sms`
+sms = Proxy('sms', facades.Sms)
+
+#: TTS proxy to :class:`plyer.facades.TTS`
+tts = Proxy('tts', facades.TTS)
+
+#: UniqueID proxy to :class:`plyer.facades.UniqueID`
+uniqueid = Proxy('uniqueid', facades.UniqueID)
+
+#: Vibrator proxy to :class:`plyer.facades.Vibrator`
+vibrator = Proxy('vibrator', facades.Vibrator)
+
+#: Flash proxy to :class:`plyer.facades.Flash`
+flash = Proxy('flash', facades.Flash)
+
+#: Wifi proxy to :class:`plyer.facades.Wifi`
+wifi = Proxy('wifi', facades.Wifi)
+
+#: Temperature proxy to :class:`plyer.facades.Temperature`
+temperature = Proxy('temperature', facades.Temperature)
