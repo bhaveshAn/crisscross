4fd45b74bae77c0e6b2ce5d53bf77b458067d960
bhaveshAn <bhaveshanand7@gmail.com>
2017-05-24 13:48:51 +0530

    added linux orientation implementation

---
+++ crisscross/platforms/linux/orientation.py
@@ -0,0 +1,41 @@
+from subprocess import call, Popen, PIPE
+from crisscross.facades import Orientation
+from crisscross.utils import whereis_exe
+
+
+class XrandrOrientation(Orientation):
+
+    def _set_landscape(self, **kwargs):
+        self.rotate = 'normal'
+        xrandr_process = Popen(["xrandr", "-q"], stdout=PIPE, stderr=PIPE)
+        grep_process_one = Popen(["grep", "connected"],
+            stdin=xrandr_process.stdout, stdout=PIPE)
+        grep_process_two = Popen(["head", "-n", "1"],
+            stdin=grep_process_one.stdout, stdout=PIPE)
+        grep_process_three = Popen(["cut", "-d", " ", "-f1"],
+            stdin=grep_process_two.stdout, stdout=PIPE)
+        xrandr_process.stdout.close()
+        output = grep_process_three.communicate()[0]
+        output = output.split()[0]
+        call(["xrandr", "--output", output, "--rotate", self.rotate])
+
+    def _set_portrait(self, **kwargs):
+        self.rotate = 'left'
+        xrandr_process = Popen(["xrandr", "-q"], stdout=PIPE, stderr=PIPE)
+        grep_process_one = Popen(["grep", "connected"],
+            stdin=xrandr_process.stdout, stdout=PIPE)
+        grep_process_two = Popen(["head", "-n", "1"],
+            stdin=grep_process_one.stdout, stdout=PIPE)
+        grep_process_three = Popen(["cut", "-d", " ", "-f1"],
+            stdin=grep_process_two.stdout, stdout=PIPE)
+        xrandr_process.stdout.close()
+        output = grep_process_three.communicate()[0]
+        output = output.split()[0]
+        call(["xrandr", "--output", output, "--rotate", self.rotate])
+
+
+def instance():
+    if whereis_exe('xrandr'):
+        return XrandrOrientation()
+    else:
+return Orientation()
